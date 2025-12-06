74292069d138715b681db1bd4001a7ae1f89f909
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2025-12-05 19:05:10 -0500

    Add Timer

--- src/main/java/simpleJGE/Timer.java
+++ src/main/java/simpleJGE/Timer.java
@@ -1,4 +1,51 @@
 package simpleJGE;
 
-public class Timer {
+import simpleJGE.abstracts.Processable;
+
+@SuppressWarnings("unused")
+// usage depends upon user implementation
+
+public abstract class Timer implements Processable {
+	private long timeLimitMillis;
+	private long startTimeMillis;
+
+	private boolean running;
+	private Runnable onTimeout;
+
+	public Timer(long timeLimitMillis) {
+		this.timeLimitMillis = timeLimitMillis;
+		start();
+	}
+
+	public Timer() {
+		this(1000);
+	}
+
+	public void start() {
+		startTimeMillis = System.currentTimeMillis();
+	}
+
+	public void setTimeLimitMillis(long timeLimitMillis) {
+		this.timeLimitMillis = timeLimitMillis;
+	}
+
+	public long getTimeElapsed() {
+		return System.currentTimeMillis() - startTimeMillis;
+	}
+
+	public long getTimeLeft() {
+		return (startTimeMillis + timeLimitMillis) - System.currentTimeMillis();
+	}
+
+	public void setOnTimeout(Runnable onTimeout) {
+		this.onTimeout = onTimeout;
+	}
+
+	@Override
+	public void update() {
+		if (getTimeLeft() < 0 && this.running) {
+			this.running = false;
+			onTimeout.run();
+		}
+	}
 }
